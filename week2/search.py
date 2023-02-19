81acf1840827516cfa23d5c43a79e8bb0f1c0058
Gabor Jozsef Stenzinger <gabor.stenzinger@gmail.com>
2023-02-19 18:28:12 +0000

    Fixing autocomplete

--- week2/search.py
+++ week2/search.py
@@ -64,8 +64,19 @@ def autocomplete():
         if prefix is not None:
             type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
             ##### W2, L3, S1
-            search_response = None
-            print("TODO: implement autocomplete AND instant search")
+            opensearch = get_opensearch()
+            q1 = {"suggest": {
+                "autocomplete": {
+                    "prefix": prefix,
+                    "completion": {         
+                        "field": "suggest"  
+                    }
+                }
+            }}
+            search_response = opensearch.search(
+                body = q1,
+                index = 'bbuy_queries' if type == "queries" else "bbuy_products"
+            )
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
     print(f"Results: {results}")
@@ -108,6 +119,7 @@ def query():
         ##### W2, L1, S2
 
         ##### W2, L2, S2
+        qu.add_spelling_suggestions(query_obj, user_query)
         print("Plain ol q: %s" % query_obj)
     elif request.method == 'GET':  # Handle the case where there is no query or just loading the page
         user_query = request.args.get("query", "*")
@@ -123,7 +135,7 @@ def query():
         #### W2, L1, S2
 
         ##### W2, L2, S2
-
+        qu.add_spelling_suggestions(query_obj, user_query)
     else:
         query_obj = qu.create_query("*", "", [], sort, sortDir, size=100)
 
--- week2/utilities/query_utils.py
+++ week2/utilities/query_utils.py
@@ -159,16 +159,30 @@ def create_query(user_query, filters, sort="_score", sortDir="desc", size=10, in
 # Give a user query from the UI and the query object we've built so far, adding in spelling suggestions
 def add_spelling_suggestions(query_obj, user_query):
     #### W2, L2, S1
-    print("TODO: IMPLEMENT ME")
-    #query_obj["suggest"] = {
-    #    "text": user_query,
-    #    "phrase_suggest": {
-
-    #    },
-    #    "term_suggest": {
-
-    #    }
-    #}
+    query_obj["suggest"] = {
+        "text": user_query,
+        "phrase_suggest": {
+            "phrase": {
+                "field": "suggest.trigrams",
+                "direct_generator":  [ {
+                    "field": "title.trigram",
+                    "suggest_mode": "popular",
+                    "min_word_length": 2
+                } ],
+                "highlight": {
+                    "pre_tag": "<em>",
+                    "post_tag": "</em>"
+                }
+            }
+        },
+        "term_suggest": {
+            "term": {
+                "field": "suggest.text",
+                "suggest_mode": "popular",
+                "min_word_length": 3
+            }
+        }
+   }
 
 
 # Given the user query from the UI, the query object we've built so far and a Pandas data GroupBy data frame,
